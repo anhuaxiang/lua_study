678c299173582ee1aa66ed5ed798c2c7d9f8456e
anhuaxiang <1342181530@qq.com>
2018-10-16 11:39:16 +0800

    feature: lua语法

--- hello.lua
+++ hello.lua
@@ -7,5 +7,62 @@ print("hello")
 ]]--
 
 
+print("nihao")
+a, b = 10, 20
+f = 20 / 3
+g = 20 / 3
+print(a, b)
+print(f)
+print(j)
+
+
+
+a = 5
+print(a)
+
+
+local a = 6
+
+function joke()
+	c = 5
+	local  a = 4
+end
+joke()
+print(a, c)
+a = a + 1
+print(a)
+print(a, b)
+
+a, b = b, a
+print(a, b)
+
+a, b, c = 10, 1
+print(a, b, c)
+a, b = 1, 2, 3, 4
+print(a, b)
+
+a = {}
+a['key'] = "value"
+print(a.key)
+
+print(type(type(abc)), abc)
+print(type(1), 1)
+print(type(1.0), 1.0)
+print(joke, type(joke))
+a, b, c = 1, 2, 1
+print(a==c, a~=b, (a==c and a~=b), not false)
+
+c, d = "a", "b"
+print(c..d)
+print(#c..d)
+
+
+if a and b
+then
+	print('ha')
+else
+	print('bb')
+end
+
 
 
